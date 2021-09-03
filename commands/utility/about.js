c5a65b7b3325a5f8eb387d7bffc9f1a3f3921145
alefnull <nullalef@gmail.com>
2021-09-03 19:53:35 -0400

    removed unused discord server field

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -12,12 +12,11 @@ module.exports = {
 			.setTitle('SpawnBot2')
 			.addFields({ name: 'Author', value: "[@alefnull](https://twitter.com/alefnull 'https://twitter.com/alefnull')", inline: true },
 				{ name: 'Version', value: 'v1.0.0', inline: true },
-				{ name: 'Git Repo', value: '<Coming Soon>', inline: false },
-				{ name: 'Discord Server', value: '<Coming Soon>', inline: false })
+				{ name: 'Git Repo', value: "[https://github.com/alefnull/SpawnBot2](https://github.com/alefnull/SpawnBot2 'https://github.com/alefnull/SpawnBot2')", inline: false })
 			.setDescription("SpawnBot2 is a poor imitation of [@xwcg's](https://twitter.com/xwcg 'https://twitter.com/xwcg') original [SpawnBot](https://github.com/xwcg/SpawnBot 'https://github.com/xwcg/SpawnBot') (AKA Herobrine)." +
 				"\n\nI began recreating it in order to learn node.js and discord.js (and to see how far GitHub Copilot could take me), and I hope to continue to improve upon it over time." +
 				`\n\nIf you have any questions or suggestions, or find any bugs, please contact ${alef.tag} on discord or on twitter [@alefnull](https://twitter.com/alefnull 'https://twitter.com/alefnull').`)
-			.setFooter('Made with ❤️ by @alefnull (and GitHub Copilot)');
+			.setFooter('Made with ❤️ by @alefnull');
 		msg.channel.send({ embeds: [embed] });
 	}
 }
