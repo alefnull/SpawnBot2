505f29614d395348127d8b45171c4eea476382b7
alefnull <nullalef@gmail.com>
2021-09-11 00:50:41 -0400

    configuring for use as installable/runnable module

--- main.js
+++ main.js
@@ -3,19 +3,22 @@ const { token } = require('./data/config.json');
 
 // https://ziad87.net/intents/ to calculate intents
 const client = new Client({ intents: 13895 });
-module.exports = client;
 
-// clear console
-console.clear();
+function start() {
+	// clear console
+	console.clear();
 
-// create collections for commands and custom commands
-client.commands = new Collection();
-client.custom_commands = new Collection();
+	// create collections for commands and custom commands
+	client.commands = new Collection();
+	client.custom_commands = new Collection();
 
-// run command and event handlers
-['commands', 'events'].forEach(handler => {
-	require(`./handlers/${handler}`)(client);
-});
+	// run command and event handlers
+	['commands', 'events'].forEach(handler => {
+		require(`./handlers/${handler}`)(client);
+	});
 
-// log the bot into discord
-client.login(token);
+	// log the bot into discord
+	client.login(token);
+}
+
+module.exports = { client, start };
